360aab24b4b223325f67f90596de702e020c8be6
julien <ju@pop-os.localdomain>
2024-03-29 15:51:13 +0100

    themes.lua: Add file + add some themes

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,13 @@
+return {
+        -- add gruvbox
+        { "alligator/accent.vim" },
+        { "ntk148v/komau.vim" },
+
+        -- Configure LazyVim to load a given theme
+        {
+                "LazyVim/LazyVim",
+                opts = {
+                        colorscheme = "komau",
+                },
+        },
+}
